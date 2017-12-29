3e0a67ae5ddbe54ea5f56f803fa5168769c4bb08
Hongyu Zhang <hongyuz@vmware.com>
2017-12-29 11:29:17 +0800

    redis integration (not completed)

--- src/main/java/hy/springmaven/controller/RestImageController.java
+++ src/main/java/hy/springmaven/controller/RestImageController.java
@@ -17,6 +17,7 @@ import hy.springmaven.service.ImageService;
 import hy.springmaven.service.ImageServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
@@ -56,8 +57,10 @@ public class RestImageController {
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@Cacheable("getImageById")
 	@ResponseBody
 	public ImageItem getImageById(@PathVariable String id) {
+		System.out.println("start to get image by id: " + id);
 		ImageItem imageItem = imageService.getImage(id);
 		return imageItem;
 	}
